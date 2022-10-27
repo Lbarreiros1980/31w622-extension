8cd343468a5b9ed432f633df4c5da74dd757a17c
Eddy Martin <eddypmartin@gmail.com>
2022-10-27 17:34:39 -0400

    c9 carrousel boite modale

--- em_carrousel.php
+++ em_carrousel.php
@@ -11,17 +11,52 @@
  * Author URI: https://github.com/eddytuto
  */
 
-function genere_html(){
+ /*
+filemtime() // retourne en milliseconde le temps de la dernière sauvegarde
+plugin_dir_path() // retourne le chemin du répertoire du plugin
+__FILE__ // le fichier en train de s'exécuter
+wp_enqueue_style() // Intègre le link:css dans la page
+wp_enqueue_script() // intègre le script dans la page
+wp_enqueue_scripts // le hook
+*/
+
+
+
+function em_enqueue()
+{
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js =  filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+wp_enqueue_style('em_carrousel_css',
+                plugin_dir_url(__FILE__) . "style.css",
+                array(),
+                $version_css,
+                false);
+
+
+wp_enqueue_script('em_carrousel_js',
+                plugin_dir_url(__FILE__) . "js/carrousel.js",
+                array(),
+                $version_js,
+                true);                
+}
+
+add_action("wp_enqueue_scripts","em_enqueue");
+
+
+
+function boite_carrousel(){
     /////////////////////////////////////// HTML
     // Le conteneur d'une boîte
     $contenu = 
-    
-    "<div class='boite'>"
-    . "<code>-------Auteur: " . get_the_author() . "</code>"
-    . "<date>-------Date de publication: " . get_the_date() . "</date>"
-    . "<code>---------Adresse URL" . get_the_guid() . "</code>"
-    . '</div> <!-- fin class="boite" -->';
+    "<button class='bouton'>Ouvrir carrousel</button>"
+     ."<div class='carrousel'>"
+        .'<button class="carrousel__x">X<button>'
+        .'<figure class="carrousel__figure"></figure>'
+        .'<form class="carrousel__form"></form>'
+    . '</div> <!-- fin class="carrousel" -->';
     
     return $contenu;
    }
-   add_shortcode('em_carrousel', 'genere_html');
+
+   add_shortcode('em_carrousel', 'boite_carrousel');
